70d2f996b27f85d57c1651145820df5557f90735
MikChanna <channa.mik@gmail.com>
2020-07-01 18:22:56 -0400

    fixed issue with ajax call

--- script.js
+++ script.js
@@ -3,19 +3,18 @@ $(document).ready(function () {
   var forecast = $(".forecast");
 
   $("#button").on("click", function () {
-    var cityName = html($("input"));
+    var cityName = $("#search").val().trim();
     var key = "da6d702ce9ba1524729fa879b5abc291";
     var queryURL =
       "https://api.openweathermap.org/data/2.5/forecast?q=" +
       cityName +
       "&appid=" +
       key;
-
     $.ajax({
       url: queryURL,
       method: "GET",
     }).then(function (response) {
-      console.log("hello");
+      console.log(cityName);
       console.log(response);
     });
   });
